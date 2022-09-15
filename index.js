512487033a588417d4739bce7e5e828399be3dec
Daniel Arany <adani32@gmail.com>
2022-09-15 21:05:14 +0200

    HWT-4: Trying ejs templating

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const app = express();
+const ejs = require("ejs");
 
 const port_num = 3000
 
+app.set("view engine", "ejs");
+
 app.use(express.static('static'));
+
+app.use("/", (req, res) => {
+    res.render("index", {asd: 'JS is garbage'});
+});
+
 app.listen(port_num, function () {
     console.log(`Running on :${port_num}`);
 });
