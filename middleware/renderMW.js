28aa03f82706757a638a887a578df1f61df77130
Daniel Arany <adani32@gmail.com>
2022-09-17 20:32:57 +0200

    HWT-4: Program runs without redirections

--- middleware/renderMW.js
+++ middleware/renderMW.js
@@ -1,11 +1,11 @@
 /**
  * Renders the page with the data fetched by other middlewares.
  */
-const requireOption = require('../requireOption');
+const requireOption = require('./requireOption');
 
-module.exports = function (objectrepository) {
+module.exports = function (objectrepository, viewName) {
     return function (req, res, next) {
-        next();
+        res.render(viewName, res.locals);
     };
 };
 
