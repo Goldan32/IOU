cb53d782d72f8d86e3645ee125fe9013738f5611
Daniel Arany <adani32@gmail.com>
2022-09-25 17:29:05 +0200

    Make routing possible

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+/* TODO: make middlewares provide data to ejs engine */
+
 const express = require('express');
 const app = express();
-const ejs = require("ejs");
+//const ejs = require("ejs");
 
 const port_num = 3000
 
@@ -10,6 +12,11 @@ app.use(express.static('static'));
 
 // Routing
 require('./route/index')(app);
+
+app.use((err, req, res, next) => {
+    res.end('Problem...');
+    console.log(err);
+});
 
 /*app.use("/", (req, res) => {
     res.render("index", {asd: 'JS is garbage'});
