85e3994e2af2dc3475a2018e72e2e20386f1ca63
Daniel Arany <adani32@gmail.com>
2022-10-08 13:05:38 +0200

    HWT-4: Finishing EJS templating

--- middleware/people/getPersonMW.js
+++ middleware/people/getPersonMW.js
@@ -6,7 +6,12 @@ const requireOption = require('../requireOption');
 module.exports = function (objectrepository) {
    return function (req, res, next) {
         let person = {_id:0, name:'Daniel', overallBalance:7000, eventBalance:5000};
-        res.local.person = person;
+        res.locals.person = person;
+        let eventList = [
+            {_id:0, name:'A cold one with the bois', eventBalance:5000},
+            {_id:1, name:'Board games', eventBalance:3000},
+        ];
+        res.locals.eventList = eventList;
         next();
     };
 };
