5ea6e7896a9cba70ce7cbaf831dc35967eebac7d
Daniel Arany <adani32@gmail.com>
2022-11-13 18:45:20 +0100

    HWT-6: Names are now unique

--- middleware/people/savePersonMW.js
+++ middleware/people/savePersonMW.js
@@ -7,7 +7,12 @@ module.exports = function (objectrepository) {
     const PersonModel = requireOption(objectrepository, 'PersonModel')
 
     return function (req, res, next) {
+        res.locals.alreadyExists = false;
         if (typeof req.body.personName === 'undefined') {
+            return next();
+        }
+        if (typeof (res.locals.people.find(p =>  p.name == req.body.personName)) !== 'undefined') {
+            res.locals.alreadyExists = true;
             return next();
         }
         if (typeof res.locals.person === 'undefined') {
